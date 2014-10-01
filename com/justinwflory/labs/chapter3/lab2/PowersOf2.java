72c05303f09f8a87b7e44c8d29006692ec913bd0
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-01 11:20:27 -0400

    Discovered how to calculate powers of 2 the tedious way

--- com/justinwflory/labs/chapter3/lab2/PowersOf2.java
+++ com/justinwflory/labs/chapter3/lab2/PowersOf2.java
@@ -6,10 +6,8 @@
 // ****************************************************************
 import java.util.Scanner;
 
-public class PowersOf2
-{
-    public static void main(String[] args)
-    {
+public class PowersOf2 {
+    public static void main(String[] args) {
         int numPowersOf2;        //How many powers of 2 to compute
         int nextPowerOf2 = 1;    //Current power of  2
         int exponent;            //Exponent for current power of 2 -- this
@@ -19,22 +17,23 @@ public class PowersOf2
         System.out.println("How many powers of 2 would you like printed?");
         numPowersOf2 = scan.nextInt();
         
-        //add code below each comment to carry out the action
+        // add code below each comment to carry out the action
         // listed in the comment; more than one statement may be needed
         // in some cases
         
-        //print a message saying how many powers of 2 will be printed
+        // print a message saying how many powers of 2 will be printed
+        System.out.println(numPowersOf2 + " powers of 2 will be printed!");
         
-        //initialize exponent -- the first thing printed is 2 to the what?
+        // initialize exponent -- the first thing printed is 2 to the what?
+        exponent = 0;
 
-        while ( /* replace this comment with appropriate condition */ )
-        {
+        while (exponent <= numPowersOf2) {
           //print out current power of 2
-          
+          System.out.println("2^" + exponent + " = "+ nextPowerOf2);
           //find next power of 2 -- how do you get this from the last one?
-          
+          nextPowerOf2 = nextPowerOf2 * 2;
           //increment exponent
-          
+          exponent++;
         }
     }
 }
