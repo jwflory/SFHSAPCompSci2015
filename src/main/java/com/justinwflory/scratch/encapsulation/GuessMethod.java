e9654002766dd3a941e0c05f5d97290abc706567
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-03 11:04:49 -0400

    Add changes from end of class

--- src/main/java/com/justinwflory/scratch/encapsulation/GuessMethod.java
+++ src/main/java/com/justinwflory/scratch/encapsulation/GuessMethod.java
@@ -17,9 +17,13 @@ public class GuessMethod {
     int snapshotGuess;  // Temporary integer used to remember the guess taken before changing it
     int numLowGuesses = 0;
     int numHighGuesses = 0;
-    boolean outOfRange = false;
+    boolean tooLow = false;
+    boolean tooHigh = false;
     
     Scanner scan = new Scanner(System.in);
+    
+    // Give a debug warning to the user running this code :)
+    System.out.println("WARNING! This code is not functional... yet.");
     
     // Randomly generate the  number to guess
     numToGuess = (int) ((Math.random() * 100) + 1);
@@ -28,7 +32,7 @@ public class GuessMethod {
     guess = readGuess();
     
     // Check to see if they went the right direction
-    if (outOfRange && guess == snapshotGuess)
+    //if (outOfRange || guess == snapshotGuess)
     
     System.out.println("Nope, that's not it. Try again.");
     snapshotGuess = guess;
@@ -59,7 +63,7 @@ public class GuessMethod {
     System.out.println("You're good.");
     System.out.println("It only took you " + guessesTaken + " times!");
   }
-  
+  /*
   public static int readGuess() {
     int numToGuess;
     int guess;
@@ -81,5 +85,23 @@ public class GuessMethod {
     }
     return guess;
   }
+  
+  public static int checkGuess (boolean tooLow, boolean tooHigh, int guess) {
+    int guess;
+    
+    Scanner scan = new Scanner(System.in);
+    
+    if (tooLow) {
+      System.out.println("Please guess higher than " + guess + ".");
+    }
+    if (tooHigh) {
+      System.out.println("Please guess lower than " + guess + ".");
+    }
+    
+    while ((tooLow && guess <= snapshotGuess) || (tooHigh && guess >= snapshotGuess)) {
+      //TODO
+      System.out.println("TODO");
+    }
+  }*/
   
 }
