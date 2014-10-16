4f16b94da5b2036118555cde744adf46701598dd
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-16 11:43:00 -0400

    Begin initializing arrays and calculate average

--- src/main/java/com/justinwflory/labs/chapter7/Sales.java
+++ src/main/java/com/justinwflory/labs/chapter7/Sales.java
@@ -7,36 +7,42 @@
 // ****************************************************************
 import java.util.Scanner;
 
-public class Sales
-{
-    public static void main(String[] args)
-    {
-
-	/* when an array is going to be a fixed size, using a constant like the one used
-         * below to define the size of the array is a good idea.  The name of the constant
-         * is a better indicator of why the array is the size that it is.
-         */
-	final int SALESPEOPLE = 5;
-	int[] sales = new int[SALESPEOPLE];
-	int sum;
-
-	Scanner scan = new Scanner(System.in);
-
-	for (int i=0; i<sales.length; i++)
-	    {
-		System.out.print("Enter sales for salesperson " + i + ": ");
-		sales[i] = scan.nextInt();
-	    }
-
-	System.out.println("\nSalesperson   Sales");
-	System.out.println("--------------------");
-	sum = 0;
-	for (int i=0; i<sales.length; i++)
-	    {
-		System.out.println("     " + i + "         " + sales[i]);
-		sum += sales[i];
-	    }
-
-	System.out.println("\nTotal sales: " + sum);
+public class Sales {
+  public static void main(String[] args) {
+    
+    /* when an array is going to be a fixed size, using a constant like the one used
+     * below to define the size of the array is a good idea.  The name of the constant
+     * is a better indicator of why the array is the size that it is.
+     */
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Please enter the total number of salespeople:");
+    int salespeople = scan.nextInt();
+    
+    int[] sales = new int[salespeople];
+    int sum = 0;
+    int maximumSales;
+    
+    for (int i=0; i<sales.length; i++) {
+      System.out.print("Enter sales for salesperson " + i + ": ");
+      sales[i] = scan.nextInt();
     }
+    
+    maximumSales = sales[0];
+    
+    System.out.println("\nSalesperson   Sales");
+    System.out.println("--------------------");
+    for (int i=0; i<sales.length; i++) {
+      System.out.println("     " + i + "         $" + sales[i]);
+      sum += sales[i];
+      if (sales[i] > maximumSales) {
+        maximumSales = i;
+      }
+    }
+    
+    System.out.println("\nTotal sales: $5" + sum);
+    System.out.println("Average sales: $" + (sum / salespeople));
+    System.out.println("\nHighest sales by Salesperson " + maximumSales + " with $" + sales[maximumSales]);
+  }
 }
