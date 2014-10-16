34a20476cd19b40e497fd961dc6f21c7a7f6b941
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-16 15:34:37 -0400

    Add neccessary components to run independently

--- src/main/java/com/justinwflory/scratch/GPATest.java
+++ src/main/java/com/justinwflory/scratch/GPATest.java
@@ -1,21 +1,25 @@
-public class GPA{
+import java.util.Scanner;
 
-     public static void main(int g){
-        if (g >= 90) {
-            gpa = 4;
-        }
-        else if (g >= 80) {
-            gpa = 3;
-        }
-        else if (g >= 70) {
-            gpa = 2;
-        }
-        else if (g >= 60) {
-            gpa = 1;
-        }
-        else {
-            gpa = 0;
-        }
-        return g;
-     }
+public class GPATest {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter the grade to be checked.");
+    int g = scan.nextInt();
+    int gpa;
+    
+    if (g >= 90) {
+      gpa = 4;
+    } else if (g >= 80) {
+      gpa = 3;
+    } else if (g >= 70) {
+      gpa = 2;
+    } else if (g >= 60) {
+      gpa = 1;
+    } else {
+      gpa = 0;
+    }
+    
+    System.out.println("GPA is " + gpa);
+  }
 }
