262393cd69f72c73c23cd7295cb5b5f41782cb8f
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-20 23:35:11 -0400

    Complete QuizGrader class, including string-to-array conversion

--- src/main/java/com/justinwflory/labs/chapter7/QuizGrades.java
+++ src/main/java/com/justinwflory/labs/chapter7/QuizGrades.java
@@ -11,17 +11,50 @@ public class QuizGrades {
     Scanner scan = new Scanner(System.in);
     int totalQuestions;
     String answerKey;
+    String studentAnswers;
+    int totalCorrect = 0;
+    float percentCorrect; // Used float to use fewer decimal places
+    boolean done = false;
+    String doneCheck;
     
     System.out.println("How many questions were on the quiz?");
     totalQuestions = scan.nextInt();
+    scan.nextLine(); // Necessary otherwise scanner doesn't move to next line for strings because of nextInt
     
-    int[] answerList = new int[totalQuestions];
+    System.out.println("Please enter the answer key, separated by spaces:");
+    answerKey = scan.nextLine();   
     
-    System.out.println("Please enter the answer key separated by spaces:");
-    answerKey = scan.next();
+    // Convert the string answerKey into an array with data split by whitespace
+    String[] answerSplit = answerKey.split("\\s+");
     
-    /*for (int i=0; i<answerList; i++) {
-      String
-    }*/
+    while (!done) {
+      System.out.println("Please enter the student's answers, separated by spaces.");
+      studentAnswers = scan.nextLine();
+      String[] studentSplit = studentAnswers.split("\\s+");
+      
+      totalCorrect = 0;
+      for (int i=0; i < totalQuestions; i++) {
+        if (answerSplit[i].equals(studentSplit[i])) {
+          totalCorrect++;
+        }
+      }
+      
+      percentCorrect = ((float) totalCorrect / totalQuestions) * 100;
+      
+      System.out.println("The student answered " + totalCorrect + " questions out of " + totalQuestions +
+                         " correctly. Their percentage of questions answered correctly was " + percentCorrect + "%.");
+      
+      System.out.println("\n\nGrade another quiz? (y/n)");
+      doneCheck = scan.nextLine();
+      while (doneCheck.charAt(0) != 'y' && doneCheck.charAt(0) != 'n') {
+        System.out.println("Invalid input! Please enter a lowercase 'y' or 'n'.");
+        doneCheck = scan.nextLine();
+      }
+      if (doneCheck.charAt(0) == 'y') {
+        done = false;
+      } else if (doneCheck.charAt(0) == 'n') {
+        done = true;
+      }
+    }
   }
 }
