5f1d15c57e63d18860686cbceafc5a388d0f160d
Justin W. Flory <jflory7.v2@gmail.com>
2014-11-14 05:17:51 -0500

    Successfully determine if the squares from magicData.txt are magic or not

--- src/main/java/com/justinwflory/assignments/a20/Square.java
+++ src/main/java/com/justinwflory/assignments/a20/Square.java
@@ -10,92 +10,105 @@
 import java.util.Scanner;
 
 public class Square {
-    int[][] square;
-
-    //--------------------------------------
-    //create new square of given size
-    //--------------------------------------
-    public Square(int size) {
-      int[][] square = new int[size][size];
+  int[][] square;
+  
+  //--------------------------------------
+  //create new square of given size
+  //--------------------------------------
+  public Square(int size) {
+    square = new int[size][size];
+  }
+  
+  //--------------------------------------
+  //return the sum of the values in the given row
+  //--------------------------------------
+  public int sumRow(int row) {
+    int sum = 0;
+    for (int i=0; i < square.length; i++) {
+      sum += square[row][i];
     }
-
-    //--------------------------------------
-    //return the sum of the values in the given row
-    //--------------------------------------
-    public int sumRow(int row) {
-      int sum = 0;
+    return sum;
+  }
+  
+  //--------------------------------------
+  //return the sum of the values in the given column
+  //--------------------------------------
+  public int sumCol(int col) {
+    int sum = 0;
+    for (int i=0; i < square.length; i++) {
+      sum += square[i][col];
+    }
+    return sum;
+  }
+  
+  //--------------------------------------
+  //return the sum of the values in the main diagonal
+  // top left down to lower right
+  //--------------------------------------
+  public int sumMainDiag() {
+    int sum = 0;
+    for (int i=0; i < square.length; i++) {
+      sum += square[i][i];
+    }
+    return sum;
+  }
+  
+  //--------------------------------------
+  //return the sum of the values in the other ("reverse") diagonal
+  // bottom left up to top right
+  //--------------------------------------
+  public int sumOtherDiag() {
+    int sum = 0;
+    for (int i=0; i < square.length; i++) {
+      sum += square[i][square.length-1-i];
+    }
+    return sum;
+  }
+  
+  //--------------------------------------
+  // return true if the square is magic (all rows, cols, and diags have
+  // same sum), false otherwise
+  // Hint: get one of the diagonal sums as the "target"
+  //  then compare the target with all other possibilities; as soon as
+  //  something doesn't match, you can return false
+  //--------------------------------------
+  public boolean magic() {
+    int target = sumMainDiag();
+    
+    if (sumOtherDiag() == target) {
       for (int i=0; i < square.length; i++) {
-        sum += square[row][i];
+        if (sumRow(i) != target) return false;
       }
-      return sum;
-    }
-
-    //--------------------------------------
-    //return the sum of the values in the given column
-    //--------------------------------------
-    public int sumCol(int col) {
-      int sum = 0;
       for (int i=0; i < square.length; i++) {
-        sum += square[i][col];
+        if (sumCol(i) != target) return false;
       }
-      return sum;
+    } else {
+      return false;
     }
-
-    //--------------------------------------
-    //return the sum of the values in the main diagonal
-    // top left down to lower right
-    //--------------------------------------
-    public int sumMainDiag() {
-      int sum = 0;
-      for (int i=0; i < square.length; i++) {
-        sum += square[i][i];
-      }
-      return sum;
+    return true;
+  }
+  
+  //--------------------------------------
+  // Read info into the square from the input stream associated with the 
+  // Scanner parameter
+  // This method is COMPLETE.  You do not need to add to or change
+  //  anything in it.
+  //--------------------------------------
+  public void readSquare(Scanner scan){
+    for (int row = 0; row < square.length; row++)
+      for (int col = 0; col < square.length; col ++)
+      square[row][col] = scan.nextInt();
+  }
+  
+  //--------------------------------------
+  // print the contents of the square, neatly formatted
+  // Hint: use nested loops and the "\t" escape character (see Section 2.2)
+  //--------------------------------------
+  public void printSquare() {
+    for (int row=0; row < square.length; row++) { 
+      for (int col=0; col < square[row].length; col++) 
+        System.out.print (square[row][col] + "\t"); 
+      System.out.println(""); 
     }
-
-    //--------------------------------------
-    //return the sum of the values in the other ("reverse") diagonal
-    // bottom left up to top right
-    //--------------------------------------
-    public int sumOtherDiag() {
-      int sum = 0;
-      for (int i=0; i < square.length; i++) {
-        sum += square[i][square.length-1-i];
-      }
-      return sum;
-    }
-
-    //--------------------------------------
-    // return true if the square is magic (all rows, cols, and diags have
-    // same sum), false otherwise
-    // Hint: get one of the diagonal sums as the "target"
-    //  then compare the target with all other possibilities; as soon as
-    //  something doesn't match, you can return false
-    //--------------------------------------
-    public boolean magic() {
-      int target = sumMainDiag();
-      
-      // More progress to be added soon.
-    }
-
-    //--------------------------------------
-    // Read info into the square from the input stream associated with the 
-    // Scanner parameter
-    // This method is COMPLETE.  You do not need to add to or change
-    //  anything in it.
-    //--------------------------------------
-    public void readSquare(Scanner scan){
-      for (int row = 0; row < square.length; row++)
-   for (int col = 0; col < square.length; col ++)
-     square[row][col] = scan.nextInt();
-    }
-
-    //--------------------------------------
-    //print the contents of the square, neatly formatted
-    // Hint: use nested loops and the "\t" escape character (see Section 2.2)
-    //--------------------------------------
-    public void printSquare() {
-
-    }
-
+  }
 }
--- src/main/java/com/justinwflory/assignments/a20/SquareTest.java
+++ src/main/java/com/justinwflory/assignments/a20/SquareTest.java
@@ -9,6 +9,9 @@
 // ****************************************************************
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SquareTest {
   public static void main(String[] args) throws IOException {
@@ -21,25 +24,36 @@ public class SquareTest {
     while (size != -1) {
       
       //create a new Square of the given size
+      Square square = new Square(size);
       
       //call its read method to read the values of the square
+      square.readSquare(scan);
       
       System.out.println("\n******** Square " + count + " ********");
       //print the square
+      square.printSquare();
+      System.out.print("\n");
       
       //print the sums of its rows
+      for (int row=0; row < size; row++) System.out.println("The sum of Row #" + row + " is:\t\t" + 
+                                                            square.sumRow(row));
       
       //print the sums of its columns
+      for (int col=0; col < size; col++) System.out.println("The sum of Column #" + col + " is:\t\t" + 
+                                                            square.sumCol(col));
       
       //print the sum of the main diagonal
+      System.out.println("The sum of the main diagonal is:\t" + square.sumMainDiag());
       
       //print the sum of the other diagonal
+      System.out.println("The sum of the other diagonal is:\t" + square.sumOtherDiag());
       
       //determine and print whether it is a magic square
+      System.out.println("Is Square #" + count + " a magic square?\t\t" + square.magic());
       
       //get size of next square
       size = scan.nextInt();
-      
+      count++;
     }
     
   }
