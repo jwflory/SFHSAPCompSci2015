b4fddaf6f5a5138ea9705679f152363bc54ffe34
Justin W. Flory <me@justinwflory.com>
2015-03-16 08:07:27 -0400

    Clean up default BoxBugRunner

--- src/main/java/com/justinwflory/labs/gridworld/projects/boxBug/BoxBugRunner.java
+++ src/main/java/com/justinwflory/labs/gridworld/projects/boxBug/BoxBugRunner.java
@@ -17,24 +17,28 @@
  */
 
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.grid.Location;
-
-import java.awt.Color;
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.*;
 
 /**
  * This class runs a world that contains box bugs. <br />
  * This class is not tested on the AP CS A and AB exams.
  */
-public class BoxBugRunner
-{
-    public static void main(String[] args)
-    {
-        ActorWorld world = new ActorWorld();
-        BoxBug alice = new BoxBug(6);
-        alice.setColor(Color.ORANGE);
-        BoxBug bob = new BoxBug(3);
-        world.add(new Location(7, 8), alice);
-        world.add(new Location(5, 5), bob);
-        world.show();
-    }
+public class BoxBugRunner implements Colors {
+  public static void main(String[] args) {
+    BoundedGrid<Actor> grid = new BoundedGrid<Actor>(20, 20);
+    ActorWorld world = new ActorWorld(grid);
+    
+    BoxBug alice = new BoxBug(6);
+    alice.setColor(allColors[(int) (Math.random() * allColors.length)]);
+    
+    BoxBug bob = new BoxBug(3);
+    bob.setColor(allColors[(int) (Math.random() * allColors.length)]);
+    
+    world.add(new Location(7, 8), alice);
+    world.add(new Location(5, 5), bob);
+    
+    world.show();
+  }
 }
