6a21c34898fe375ab97de110c54113889c2ca489
Justin W. Flory <me@justinwflory.com>
2015-03-21 19:44:38 -0400

    Move BoxBug files into their own project

---
+++ src/main/java/com/justinwflory/labs/gridworld/projects/shapeBug/Colors.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+public interface Colors {
+  Color[] allColors = {
+      Color.BLACK,
+      Color.BLUE,
+      Color.CYAN,
+      Color.DARK_GRAY,
+      Color.GRAY,
+      Color.GREEN,
+      Color.LIGHT_GRAY,
+      Color.MAGENTA,
+      Color.ORANGE,
+      Color.PINK,
+      Color.RED,
+      Color.WHITE,
+      Color.YELLOW
+    };
+}
