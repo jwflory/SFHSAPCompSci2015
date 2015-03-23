51889c24b732f253fa56979a12fde1225cba8736
Justin W. Flory <me@justinwflory.com>
2015-03-23 11:07:44 -0400

    Add runner for all bugs

--- src/main/java/com/justinwflory/labs/gridworld/projects/shapeBug/ShapeBugRunner.java
+++ src/main/java/com/justinwflory/labs/gridworld/projects/shapeBug/ShapeBugRunner.java
@@ -6,18 +6,18 @@ import info.gridworld.grid.*;
 /**
  * This class runs a world that contains all bugs.
  */
-public class ShapeRunner implements Colors {
-  private static final boolean SAFETY = false;
+public class ShapeBugRunner implements Colors {
+  private static final boolean SAFETY = true;
   
   public static void main(String[] args) {
     UnboundedGrid<Actor> grid = new UnboundedGrid<Actor>();
     ActorWorld world = new ActorWorld(grid);
     
-    BoxBug alice = new BoxBug(6);
-    alice.setColor(colors[(int) (Math.random() * colors.length)]);
+    BoxBug box1 = new BoxBug(6);
+    box1.setColor(colors[(int) (Math.random() * colors.length)]);
     
-    BoxBug bob = new BoxBug(3);
-    bob.setColor(colors[(int) (Math.random() * colors.length)]);
+    BoxBug box2 = new BoxBug(3);
+    box2.setColor(colors[(int) (Math.random() * colors.length)]);
     
     CircleBug circle1 = new CircleBug(4);
     circle1.setColor(colors[(int) (Math.random() * colors.length)]);
@@ -47,8 +47,15 @@ public class ShapeRunner implements Colors {
     custom2.setColor(colors[(int) (Math.random() * colors.length)]);
     Rock customRock = new Rock(colors[(int) (Math.random() * colors.length)]);
     
-    world.add(new Location(7, 8), alice);
-    world.add(new Location(5, 5), bob);
+    NewCustomBug newCustom1 = new NewCustomBug(5);
+    newCustom1.setColor(colors[(int) (Math.random() * colors.length)]);
+    
+    NewCustomBug newCustom2 = new NewCustomBug(14);
+    newCustom2.setColor(colors[(int) (Math.random() * colors.length)]);
+    Rock newCustomRock = new Rock(colors[(int) (Math.random() * colors.length)]);
+    
+    world.add(new Location(7, 8), box1);
+    world.add(new Location(5, 5), box2);
     world.add(new Location(15, 20), circle1);
     world.add(new Location(60, 20), circle2);
     world.add(new Location(74, 41), circleRock);
@@ -61,6 +68,9 @@ public class ShapeRunner implements Colors {
     world.add(new Location(25, 5), custom1);
     world.add(new Location(70, 70), custom2);
     world.add(new Location(22, 5), customRock);
+    world.add(new Location(90, 70), newCustom1);
+    world.add(new Location(70, 90), newCustom2);
+    world.add(new Location(80, 80), newCustomRock);
     if (SAFETY) for (int i=0; i<200; i++) world.add(new Location(0, i), new Rock());
 
     world.show();
